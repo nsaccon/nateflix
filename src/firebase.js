89894ad0016e689d7476691f1f5a43dbfaadc897
Nathan <nathansaccon10@hotmail.com>
2021-04-23 11:51:30 -0400

    Nav Bar Added

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlBxlsV4_saJhCfWKRTiHXbggR2Q6EDgI",
+    authDomain: "nateflix-build.firebaseapp.com",
+    projectId: "nateflix-build",
+    storageBucket: "nateflix-build.appspot.com",
+    messagingSenderId: "272725222636",
+    appId: "1:272725222636:web:f76aa765087a34482ab1c9"
+  };
